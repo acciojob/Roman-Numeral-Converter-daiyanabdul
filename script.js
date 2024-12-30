c36e31b833b11e2cc9971fbfec85e06321f8afeb
daiyanabdul <daiyanabdul1@gmail.com>
2024-12-30 18:07:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
 function convertToRoman(num) {
-	if(num===0) return'';
-  	const obj = [
-      ['M',1000], 
-      ['D', 500], 
-      ['C', 100], 
-      ['L', 50], 
-      ['X', 10], 
-      ['V', 5], 
-      ['I', 1]
+    if (num === 0) return ''; // Special case for 0 as Roman numerals don't represent it
+
+    const obj = [
+        ['M', 1000], 
+        ['CM', 900], 
+        ['D', 500], 
+        ['CD', 400], 
+        ['C', 100], 
+        ['XC', 90], 
+        ['L', 50], 
+        ['XL', 40], 
+        ['X', 10], 
+        ['IX', 9], 
+        ['V', 5], 
+        ['IV', 4], 
+        ['I', 1]
     ];
 
-  //your code here
-	 let romanNumerals = '';
-    for(let i=0;i<obj.length;i++){
-		const[symbol,value]=obj[i];
-		while (num>=value) {
-			romanNumerals+=symbol;
-			num-=value;
-		}
-	}
-	return romanNumerals;
+    let romanNumerals = '';
+
+    for (let i = 0; i < obj.length; i++) {
+        const [symbol, value] = obj[i];
+        while (num >= value) {
+            romanNumerals += symbol;
+            num -= value;
+        }
+    }
+
+    return romanNumerals;
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
- console.log(convertToRoman(36));
+// Test the function with multiple cases
+console.log(convertToRoman(14));      
+console.log(convertToRoman(798));    
+console.log(convertToRoman(36));      
+console.log(convertToRoman(0));       
+console.log(convertToRoman(3999));    
+console.log(convertToRoman(100000));  
 
 
 
