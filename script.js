b0e9fc8dc2a9bf970bfccef97c1116d3b44748cd
daiyanabdul <daiyanabdul1@gmail.com>
2024-12-30 16:54:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,11 +10,36 @@ function convertToRoman(num) {
     };
 
   //your code here
+	 let roman = "";
+    let keys = Object.keys(obj);
+
+    for (let i = 0; i < keys.length; i++) {
+        let [symbol, value] = obj[keys[i]];
+
+        // Check for subtraction cases (like IV, IX, etc.)
+        if (i % 2 === 0 && i + 2 < keys.length) {
+            let [nextSymbol, nextValue] = obj[keys[i + 2]];
+            if (num >= value - nextValue) {
+                roman += nextSymbol + symbol;
+                num -= (value - nextValue);
+                continue;
+            }
+        }
+
+        // Add the symbols to the result while reducing the number
+        while (num >= value) {
+            roman += symbol;
+            num -= value;
+        }
+    }
+
+    return roman;
+}
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+ console.log(convertToRoman(36));
 
 
 
